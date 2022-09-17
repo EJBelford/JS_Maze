8b3455c649001c68184cd62023233e6aacef4603
Gene Belford <35238105+EJBelford@users.noreply.github.com>
2022-09-17 17:21:21 -0400

    Updates for 9/17
    Updates for 9/17

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 //--*----|----*----|----*----|----*----|----*----|----*----|----*----|----*----/
 //
 // Section 22: Section 22: Javascript with Canvas API
-// Lesson: 268
+// Lesson: 274
 //
 //--*----1----*----2----*----3----*----4----*----5----*----6----*----7----*----8
 // NOTES: 
@@ -20,7 +20,10 @@
 
 // console.log('Hi there!');
 
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, Mouse, MouseConstraint } = Matter; 
+
+const width =  800;
+const height = 600;
 
 const engine    = Engine.create();
 const { world } = engine;
@@ -28,16 +31,57 @@ const render    = Render.create({
     element: document.body,
     engine:  engine,
     options: {
-        width:  800,
-        height: 600
+        wireframes: false, 
+        width,
+        height
     }
 });
 
 Render.run(render);
-Render.run(Runner.create(), engine); 
+Runner.run(Runner.create(), engine); 
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
+World.add(world, 
+    MouseConstraint.create(engine, {
+        mouse: Mouse.create(render.canvas)
+    })
+);
+
+// Walls
+const walls = [
+    Bodies.rectangle(400,   0, 800,  40, { isStatic: true }),
+    Bodies.rectangle(400, 600, 800,  40, { isStatic: true }),
+    Bodies.rectangle(  0, 300,  40, 600, { isStatic: true }),
+    Bodies.rectangle(800, 300,  40, 600, { isStatic: true })
+];
+World.add(world, walls);
+
+/* const shape = Bodies.rectangle(200, 200, 50, 50, {
     isStatic: true
 }); 
-World.add(world, shape);
+World.add(world, shape); */
+
+// Random Shapes
+
+for (let i = 0; i < 40; i++) {
+    if (Math.random() > .5) {
+        World.add(world, 
+            Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+        );
+    } else {
+        World.add(world, 
+            Bodies.circle(
+                Math.random() * width,
+                Math.random() * height, 
+                35, 
+                {
+                    render: {
+                        fillStyle: 'red'
+                  }
+                }
+            )
+        );
+    }
+
+};
+
 
